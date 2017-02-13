90aa8b037a4dfcec5d625bd81d89722637c47c91
Joseph McCormick <esmevane@gmail.com>
2017-02-12 19:55:18 -0600

    Remove Sidebar.

--- client/components/AppContainer/index.js
+++ client/components/AppContainer/index.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react"
 import Header from "components/Header"
-import Sidebar from "components/Sidebar"
 import Home from "components/Home"
 import ErrorMessage from "components/ErrorMessage"
 import styles from "./styles.module.css"
 
 export default class AppContainer extends Component {
   render() {
-    const { header, sidebar, content } = this.props
+    const { header, content } = this.props
 
     return(
       <div className={ styles.container }>
@@ -15,15 +14,10 @@ export default class AppContainer extends Component {
           { header || <Header /> }
         </header>
         <section className={ styles.content }>
-          <aside className={ styles.sidebar }>
-            { sidebar || <Sidebar /> }
-          </aside>
-          <section className={ styles.main }>
-            <section className={ styles.notices }>
-              <ErrorMessage />
-            </section>
-            { content || <Home /> }
+          <section className={ styles.notices }>
+            <ErrorMessage />
           </section>
+          { content || <Home /> }
         </section>
       </div>
     )
